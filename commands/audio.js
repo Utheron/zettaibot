bc86fa02faa6501b4b242cb50a2fc484adc02159
Utheron <git@utheron.com>
2019-06-09 16:58:47 +0200

    TODO added

--- commands/audio.js
+++ commands/audio.js
@@ -119,6 +119,8 @@ exports.run = async (bot, message, args, guild) => {
                             let match = qAnswer.reduce((r,v) => uAnswer.includes(v) && r.concat(v, " ") || r, "")
                             
                             if (match) {
+                                // TODO: Delay the end of the audio
+
                                 // RichEmbed for right answer
                                 const embed = new Discord.RichEmbed()
                                 .setColor(color.right)
@@ -131,6 +133,7 @@ exports.run = async (bot, message, args, guild) => {
                                 return trigger = true;
                             }
                             // If the answer is wrong, do something
+                            // TODO: Add a skip/stop command
                         })
                         .catch(e => {
                             // RichEmbed for Time Up
